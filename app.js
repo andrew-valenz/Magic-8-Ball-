015710ee2ce211ddf771fb67205b418197667eb7
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-10-27 14:58:48 -0700

    Added the random ints and consts in js

--- app.js
+++ app.js
@@ -5,7 +5,41 @@ const promptSection = document.getElementById('prompt');
 const fortuneSection = document.getElementById('fortune');
 const answerP = document.getElementById('answer');
 /* Events */
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
 
-/* Display Functions */
+function toggleSelections() {
+    promptSection.classlist.toggle('hide');
+    fortuneSection.classlist.toggle('hide');
+}
+submitBtn.addEventListener('click', () => {
+    toggleSelections();
 
-// (don't forget to call any display functions you want to run on page load!)
+    //generate a random number between 0 and the length
+    // of the array minus 1
+    const randInt = Math.floor(Math.random() * answers.length);
+
+    // set the text content to the random choice
+    answerP.textContent = randomChoice;
+});
+
+resetBtn.addEventListener('click', toggleSections);
